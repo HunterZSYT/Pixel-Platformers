1058ca8b8bc1ab2a9328896c58ec890ae6e8fddc
HunterZSYT <junaed.saimon1998@gmail.com>
2023-10-15 00:01:24 +0600

    v.4 Pixel Platformers
    Updates:
    
    Added a "winning platform" at the top of the screen.
    Updated the game loop to check if the player reaches this platform.
    If the player reaches the winning platform, a "You Won!" message is displayed at the center of the screen.

--- main_game.py
+++ main_game.py
@@ -6,10 +6,11 @@ from platform import Platform
 # Initialize pygame
 pygame.init()
 
-# Define screen dimensions and colors
+# Define screen dimensions, colors, and fonts
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SKY_BLUE = (135, 206, 250)
+font = pygame.font.Font(None, 74)
 
 # Setup game screen and clock
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -26,13 +27,15 @@ platforms = pygame.sprite.Group()
 platform1 = Platform(100, 450, 200, 20)
 platform2 = Platform(400, 350, 200, 20)
 platform3 = Platform(150, 250, 200, 20)
+winning_platform = Platform(350, 100, 100, 20)
 
 # Add platforms to the sprite groups
-platforms.add(platform1, platform2, platform3)
-all_sprites.add(platform1, platform2, platform3)
+platforms.add(platform1, platform2, platform3, winning_platform)
+all_sprites.add(platform1, platform2, platform3, winning_platform)
 
 # Main game loop
 running = True
+has_won = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -59,9 +62,18 @@ while running:
             player.change_y = 0
             player.can_jump = True
 
+    # Check for winning condition
+    if player.rect.colliderect(winning_platform.rect):
+        has_won = True
+
     # Draw everything
     screen.fill(SKY_BLUE)
     all_sprites.draw(screen)
+    
+    # Display winning message
+    if has_won:
+        win_text = font.render("You Won!", True, (255, 255, 255))
+        screen.blit(win_text, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 50))
 
     pygame.display.flip()
     clock.tick(60)
